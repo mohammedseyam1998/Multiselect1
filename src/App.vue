624d027846ef9a045c262e3f1942bc7a4b21dfc5
mohammedseyam1998 <m05921033@gmail.com>
2020-01-20 17:04:31 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    
+    <multi></multi>
+    
+    <footer>Mohammed a seyam</footer>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Multi from './components/Multi.vue'
+import { MultiSelectPlugin } from "@syncfusion/ej2-vue-dropdowns";
+// import PerfectScrollbar from 'perfect-scrollbar';
+// const ps = new
+//         PerfectScrollbar('.e-multiselect.e-input-group.e-control-wrapper', {
+//         wheelSpeed: 2,
+//         wheelPropagation: true,
+//         minScrollbarLength: 20
+//         });
+// ps.update();
+
+
+
+Vue.use(MultiSelectPlugin);
+
+export default {
+  name: 'app',
+  
+  components: {
+    Multi
+  }
+}
+</script>
+
+<style>
+
+
+
+@import "../node_modules/@syncfusion/ej2-base/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-inputs/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-vue-dropdowns/styles/material.css";
+footer{
+  background-color: brown;
+  color: aliceblue;
+  text-align: center;
+  margin: 2px;
+  padding: 5px
+}
+.control_wrapper{
+   position: relative;
+    width: 500px;
+    height: 300px;
+  
+}
+
+.e-multi-select-wrapper{
+  padding: 1px;
+}
+
+.e-multiselect e-input-group e-control-wrapper{
+  direction: rtl;
+}
+.e-multi-select-wrapper .e-searcher{
+  float: left;
+  width: 100% ;
+}
+input.e-dropdownbase{
+  direction: rtl;
+}
+
+</style>
+
---
+++ src/components/Multi.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="Multi">
+    <h2>This is a MultiSelect</h2>
+    <div  class="control_wrapper" style="margin:100px auto ; width:300px"   >
+        <ejs-multiselect id='multiselect' :dataSource='sportsData' placeholder="Select Your Languages" ></ejs-multiselect>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'Multi',
+  data(){
+    return{
+      sportsData: ['Arabic', 'English', 'French', 'chinease', 'spanish','germane','italy','maxican','ggermane','iitaly','mmaxican']
+    }
+
+  },
+ 
+}
+</script >
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div{
+  margin: 50px ;
+  text-align: center;
+}
+h2{
+  color: brown;
+}
+
+
+
+</style>
